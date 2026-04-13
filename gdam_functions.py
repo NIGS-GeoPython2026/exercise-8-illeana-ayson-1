baf208732e6fb7755f2b7737b82dcf338e9a29d4
Illeana Ayson <aysonilleana@gmail.com>
2026-04-14 02:14:17 +0800

    Add files via upload

--- gdam_functions.py
+++ gdam_functions.py
@@ -1,9 +1,33 @@
 """Functions used in Exercise 8 of Geol 197 GDAM"""
 
 # Import any modules needed in your functions here
-import math
+import math as m
+import numpy as np
 
 # Define your new functions below
-def mean(x):
-    sum(mea
+def gaussian(mean, stddev, x):
+    """
+    Function for calculating 
 
+    Parameters
+    ------------
+    x: <list>
+        def
+    mean: <numerical>
+        def
+
+    stddev: <numerical>
+        def
+
+    Returns
+    -------------
+    <float>
+        def
+    """
+    normal = []
+    for y in x:
+        gauss = (np.exp(-(y-mean)**2/(2*stddev**2)))/(stddev*np.sqrt(2*np.pi))
+        normal.append(gauss)
+    return normal
+
+    
