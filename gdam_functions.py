d76556137274277d6c6cc6c1b68ed62a5b322a4b
Illeana Ayson <aysonilleana@gmail.com>
2026-04-14 12:56:50 +0800

    Add files via upload

--- gdam_functions.py
+++ gdam_functions.py
@@ -6,28 +6,38 @@ import numpy as np
 
 # Define your new functions below
 def gaussian(mean, stddev, x):
+    
     """
-    Function for calculating 
+    Function for calculating gaussian distribution
 
     Parameters
     ------------
     x: <list>
-        def
+        list of values for which the normal distribution will be calculated
     mean: <numerical>
-        def
-
+        average
     stddev: <numerical>
-        def
+        standard deviation
 
     Returns
     -------------
-    <float>
-        def
+    <list>
+        list of calculated normal distribution for x
     """
+    
+    #create new list to store calculated values
     normal = []
+    
+    #iterate for all values in x
     for y in x:
+        
+        #gaussian function formula
         gauss = (np.exp(-(y-mean)**2/(2*stddev**2)))/(stddev*np.sqrt(2*np.pi))
+        
+        #add to list
         normal.append(gauss)
+        
+    #return list of calculated values
     return normal
 
     
